ce8be60ec37cf9b212ca45a26d7d8e5229ac2a13
LuigiLasso <llasso05@gmail.com>
2025-03-18 17:00:29 -0500

    added the cleaning functions to the EDA notebook

--- notebooks/src/DataPreprocessor.py
+++ notebooks/src/DataPreprocessor.py
@@ -6,15 +6,16 @@ class DataPreprocessor:
         self.df_fact = df_fact
         self.df_dim = df_dim
 
-    def unpivot_columns(self, id_vars, value_vars, var_name="Year", value_name="Popullation"):
+    def unpivot_columns(self, id_vars, value_vars):
         # unpivot table to get years as columns
         # id_vars are columns to keep fixed
         # value vars are the columns to be unpivoted
-        self.df_fact = pd.melt(self.df_fact, id_vars, value_vars, var_name, value_name)
+        self.df_fact = pd.melt(self.df_fact, id_vars=id_vars, value_vars=value_vars, var_name="Year", value_name="Popullation")
         return self.df_fact
     
-    def join_data(self, join_key, how="left"):
+    def join_data(self, df_cleaned, join_key, how="left"):
         # Joins country dim data
-        self.df_fact.merge(self.df_dim, on=join_key, how=how)
+        self.df_fact = df_cleaned.merge(self.df_dim, on=join_key, how=how)
         return self.df_fact
 
+    
